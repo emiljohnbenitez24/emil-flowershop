7dd700f41a56bb78e8929942ba2ea69a14e0738f
Emil John <kriempots@gmail.com>
2020-01-21 14:26:25 +0800

    my sixth commit

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Nav, Navbar, NavItem, Jumbotron } from 'react-bootstrap';
 import { useSelector } from 'react-redux'
 import styled from 'styled-components';
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { TiShoppingCart } from 'react-icons/ti'
 import logo from '../assets/logo.png'
 
@@ -10,21 +10,15 @@ import './nav.css';
 
 
 const Styles = styled.div`
-
-    .navbar-brand, .navbar-nav .nav-link {
-        color: black;
+    .navbar-nav .nav-link {
+        color:black !important;
         font-family: 'Bangers';
         font-size:3vh;
     &:hover {
         color:grey;
     }
     &:focus{
-        color:#ff8c00;
-        outline:none;
-    }
-    .cart-count{
-        float:right;
-        font-size:15px;
+        color:orange !important;
     }
     }
     `
@@ -36,25 +30,26 @@ const NavigationBar = () => {
     const cart = useSelector(state => state.cart.cart)
     return (
         <Styles>
-            <Navbar bg="light" variant="dark" >
-                <Navbar.Brand> <Nav.Link as={Link} to="/" tabIndex="1"
-                    style={{ fontSize: '40px', color: '#00008b' }}>
-                    <img
-                        src={logo}
-                        width="100"
-                        height="100"
-                        className="d-inline-block align-top-left"
-                    />
-                </Nav.Link></Navbar.Brand>
+            <Navbar collapseOnSelect bg="light" expand="lg" variant="dark" >
+                <Navbar.Brand className="navbar-brand">
+                    <Nav.Link href="#home" as={NavLink} to="/"
+                    >
+                        <img
+                            src={logo}
+                            width="100"
+                            height="100"
+                            className="d-inline-block align-top-left"
+                        />
+                    </Nav.Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"
                     style={{ background: 'grey' }}
                 />
-                <Navbar.Collapse id="responsive-navbar-nav"  >
+                <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
                     </Nav>
-                    <Nav className="nav-group">
-                        <Nav.Link as={Link} to="/shop"  >Shop</Nav.Link>
-                        <Nav.Link as={Link} to="/cart"  ><TiShoppingCart /><p className="cart-count">{cart.length}</p></Nav.Link>
+                    <Nav>
+                        <Nav.Link href="#shop" as={NavLink} to="/shop"  >Shop</Nav.Link>
+                        <Nav.Link href="#cart" as={NavLink} to="/cart" ><TiShoppingCart /><p className="cart-count">{cart.length}</p></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar >
